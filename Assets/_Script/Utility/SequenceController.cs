1572362348d0d048acaed6ccdef9ddaf27c7bc2e
Farhan Muhammad <farhnmh@gmail.com>
2021-09-18 13:23:57 +0700

    Update Version Wira 12 Xime 14

--- Assets/_Script/Utility/SequenceController.cs
+++ Assets/_Script/Utility/SequenceController.cs
@@ -11,8 +11,10 @@ public enum SequenceEnum
 {
     NORMAL,
     CAUTION,
+    CAUTIONANIMATION,
     ANIMATION,
     NOTE,
+    NOTEANIMATION,
     ADDITION
 }
 
@@ -38,10 +40,13 @@ public class SequenceController : MonoBehaviour
     public SequencesData[] sequencesDatas;
     public GameObject sequenceTextNormal;
     public GameObject sequenceTextCaution;
+    public GameObject sequenceTextCautionAnimation;
     public GameObject sequenceTextAnimation;
     public GameObject sequenceTextNote;
+    public GameObject sequenceTextNoteAnimation;
     public GameObject sequenceTextAddition;
     public GameObject hyperlinkButton;
+    public GameObject hyperlinkOnAnimationButton;
     public GameObject contentContainerParent;
     public GameObject additionContainerParent;
     public GameObject objectSpawnParent;
@@ -156,11 +161,11 @@ public class SequenceController : MonoBehaviour
 
                 if (sequencesDatas[sequenceNow].hyperlink)
                 {
-                    GameObject hyperlink = Instantiate(hyperlinkButton, contentContainerParent.transform);
-                    sequencesDatas[sequenceNow].thisHyperlink = hyperlink.GetComponent<Button>();
+                    GameObject hyperlinkNormal = Instantiate(hyperlinkButton, contentContainerParent.transform);
+                    sequencesDatas[sequenceNow].thisHyperlink = hyperlinkNormal.GetComponent<Button>();
 
-                    hyperlink.transform.localPosition = new Vector3(0, -50, 0);
-                    hyperlink.transform.localScale = new Vector3(3, 3, 3);
+                    hyperlinkNormal.transform.localPosition = new Vector3(0, -50, 0);
+                    hyperlinkNormal.transform.localScale = new Vector3(3, 3, 3);
 
                     button = sequencesDatas[sequenceNow].thisHyperlink;
                 }
@@ -171,18 +176,56 @@ public class SequenceController : MonoBehaviour
                 cautionText.text = sequencesDatas[sequenceNow].sequenceDetails;
                 SoundManager();
                 break;
+            case SequenceEnum.CAUTIONANIMATION:
+                TextMeshProUGUI cautionAnimationText = Instantiate(sequenceTextCautionAnimation, contentContainerParent.transform)
+                    .GetComponentInChildren<TextMeshProUGUI>();
+                cautionAnimationText.text = sequencesDatas[sequenceNow].sequenceDetails;
+                Instantiate(sequencesDatas[sequenceNow].sequenceAnimation, objectSpawnParent.transform);
+                SoundManager();
+
+                if (sequencesDatas[sequenceNow].hyperlink)
+                {
+                    GameObject hyperlinkAnimation = Instantiate(hyperlinkOnAnimationButton, contentContainerParent.transform);
+                    sequencesDatas[sequenceNow].thisHyperlink = hyperlinkAnimation.GetComponent<Button>();
+
+                    button = sequencesDatas[sequenceNow].thisHyperlink;
+                }
+                break;
             case SequenceEnum.ANIMATION:
                 TextMeshProUGUI animationText = Instantiate(sequenceTextAnimation, contentContainerParent.transform)
                     .GetComponentInChildren<TextMeshProUGUI>();
                 animationText.text = sequencesDatas[sequenceNow].sequenceDetails;
                 Instantiate(sequencesDatas[sequenceNow].sequenceAnimation, objectSpawnParent.transform);
                 SoundManager();
+
+                if (sequencesDatas[sequenceNow].hyperlink)
+                {
+                    GameObject hyperlinkAnimation = Instantiate(hyperlinkOnAnimationButton, contentContainerParent.transform);
+                    sequencesDatas[sequenceNow].thisHyperlink = hyperlinkAnimation.GetComponent<Button>();
+
+                    button = sequencesDatas[sequenceNow].thisHyperlink;
+                }
                 break;
             case SequenceEnum.NOTE:
                 TextMeshProUGUI noteText = Instantiate(sequenceTextNote, contentContainerParent.transform)
                     .GetComponentInChildren<TextMeshProUGUI>();
                 noteText.text = sequencesDatas[sequenceNow].sequenceDetails;
                 SoundManager();
+                break;
+            case SequenceEnum.NOTEANIMATION:
+                TextMeshProUGUI noteAnimationText = Instantiate(sequenceTextNoteAnimation, contentContainerParent.transform)
+                    .GetComponentInChildren<TextMeshProUGUI>();
+                noteAnimationText.text = sequencesDatas[sequenceNow].sequenceDetails;
+                Instantiate(sequencesDatas[sequenceNow].sequenceAnimation, objectSpawnParent.transform);
+                SoundManager();
+
+                if (sequencesDatas[sequenceNow].hyperlink)
+                {
+                    GameObject hyperlinkAnimation = Instantiate(hyperlinkOnAnimationButton, contentContainerParent.transform);
+                    sequencesDatas[sequenceNow].thisHyperlink = hyperlinkAnimation.GetComponent<Button>();
+
+                    button = sequencesDatas[sequenceNow].thisHyperlink;
+                }
                 break;
             case SequenceEnum.ADDITION:
                 TextMeshProUGUI additionText = Instantiate(sequenceTextAnimation, contentContainerParent.transform)
